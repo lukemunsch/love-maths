4d288a39f7faa6f38c2abb4a306d8dd16dc4044e
lukemunsch <lpmunsch@gmail.com>
2021-08-19 10:40:51 +0000

    add event listeners to buttons to perform designated action

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,22 @@
+//Wait for the DOM to finish loading before running the game
+// Get the button elements and add eent listeners to them
+
+document.addEventListener("DOMContentLoaded", function(){
+    let buttons = document.getElementsByTagName("button");
+
+ //   ---for(let i = 0; i < buttons.length; i++){ can be modernised for simplicity
+    for( let button of buttons){
+        button.addEventListener("click", function(){
+            if(this.getAttribute("data-type") === "submit"){
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`)
+            }
+        })
+    }
+})
+
 function runGame(){
 
 }
@@ -27,5 +46,5 @@ function displaySubtractQuestion(){
 }
 
 function displayMultiplyQuestion(){
-    
+
 }
