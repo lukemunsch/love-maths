e01fd23a6ba79bb21e5f45e49dc4000f23ac2a5e
lukemunsch <lpmunsch@gmail.com>
2021-08-19 14:38:00 +0000

    add code to increment the score counters upon submitting and answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,8 +46,10 @@ function checkAnswer(){
     let isCorrect = userAnswer === calculatedAnswer[0];
     if (isCorrect){
         alert("Hey! You got it Right! :D");
+        incrementScore();
     } else {
         alert(`Awww... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -69,12 +71,22 @@ function calculateCorrectAnswer(){
         throw `Unimplemented operator ${operator}. Aborting!`;
     }
 }
-
+/**
+ * This gets the current score from the DOM and increments it by 1
+ */
 function incrementScore(){
 
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 }
 
+/**
+ * This will also get current score from DOM and increment by 1
+ */
 function incrementWrongAnswer(){
+
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 
 }
 
